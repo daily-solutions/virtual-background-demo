2996604023a784b1aef240b47fffe4c579ec66ea
James Hush <james@daily.co>
2023-02-08 19:11:27 +0800

    Deepgram

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,16 @@ import Daily, {
 } from "@daily-co/daily-js";
 
 import {
-  useDaily,
-  useDevices,
-  useDailyEvent,
-  useScreenShare,
-  DailyVideo,
-  useParticipantIds,
   DailyAudio,
+  DailyVideo,
+  useDaily,
+  useDailyEvent,
+  useDevices,
   useInputSettings,
   useNetwork,
+  useParticipantIds,
+  useScreenShare,
+  useTranscription,
 } from "@daily-co/daily-react";
 
 import "./styles.css";
@@ -26,6 +27,56 @@ console.log("Daily version: %s", Daily.version());
 export default function App() {
   const callObject = useDaily();
   const participantIds = useParticipantIds();
+
+  const {
+    isTranscribing,
+    startTranscription,
+    stopTranscription,
+    error,
+    transcriptions,
+    isTranscriptionEnabled,
+  } = useTranscription({
+    onTranscriptionError(ev) {
+      console.log("Transcription error (daily-react)", ev);
+    },
+    onTranscriptionStarted(ev) {
+      console.log("Transcription started (daily-react)", ev);
+    },
+    onTranscriptionStopped(ev) {
+      console.log("Transcription stopped (daily-react)", ev);
+    },
+  });
+
+  // console.log("isTranscriptionEnabled: ", isTranscriptionEnabled);
+
+  if (error) {
+    console.log("Transcription error: ", error);
+  }
+
+  function enableTranscription() {
+    console.log("enableTranscription called");
+    if (!callObject) {
+      return;
+    }
+
+    startTranscription({
+      model: "video",
+    });
+
+    // callObject.startTranscription({
+    //   model: "video",
+    // });
+  }
+
+  function disableTranscription() {
+    console.log("disableTranscription called");
+    if (!callObject) {
+      return;
+    }
+
+    stopTranscription();
+    // callObject.stopTranscription();
+  }
 
   const queryParams = new URLSearchParams(window.location.search);
   const room = queryParams.get("room");
@@ -116,6 +167,8 @@ export default function App() {
       .join({
         // Replace with your own room url
         url: `https://${room}`,
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyIjoic2Z1IiwibyI6dHJ1ZSwiZCI6IjY4M2I4OWY0LWIxZmQtNGY5NC1iODQ1LWVmMjY2NmUzZTIxMyIsImlhdCI6MTY3NTg1MDc2NX0._BBaEDOGLtVQ1Lfo8hQ9hA0d9RgihemUfpVWfToSGvE",
       })
       .catch((err) => {
         console.error("Error joining room:", err);
@@ -345,7 +398,13 @@ export default function App() {
         <button onClick={() => preAuth()}>Preauth</button> <br />
         <button onClick={() => startCamera()}>Start Camera</button> <br />
         <button onClick={() => stopCamera()}>Publish Camera Off</button> <br />
-        <button onClick={() => updateCameraOn()}>Publish Camera On</button>{" "}
+        <button onClick={() => updateCameraOn()}>Publish Camera On</button>
+        <button onClick={() => enableTranscription()}>
+          Enable transcription
+        </button>
+        <button onClick={() => disableTranscription()}>
+          Disable transcription
+        </button>
         <br />
         <br />
       </div>
@@ -353,12 +412,17 @@ export default function App() {
         <DailyVideo type="video" key={id} automirror sessionId={id} />
       ))}
       <DailyAudio />
-
       <div id="meetingState">Meeting State: {callObject?.meetingState()}</div>
       {inputSettingsUpdated && <div>Input settings updated</div>}
       {errorMsg && <div id="errorMsg">{errorMsg}</div>}
       <div id="participantCount">Participant Counts: {participantCounts}</div>
       <div>Network quality: {network.quality}</div>
+      <div>{!isTranscribing ? "Not" : ""} transcribing</div>
+      <span>
+        {transcriptions.map((t) => {
+          return <div key={t.timestamp}>{t.text}</div>;
+        })}
+      </span>
     </>
   );
 }
