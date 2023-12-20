398754b0735b71b0528b46833a39fe673bfbfc06
James Hush <james@daily.co>
2023-12-20 23:22:28 +0800

    Manual track user data example

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,10 @@ const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <StrictMode>
-    <DailyProvider dailyConfig={{ useDevicePreferenceCookies: true }}>
+    <DailyProvider
+      dailyConfig={{ useDevicePreferenceCookies: true }}
+      subscribeToTracksAutomatically={false}
+    >
       <App />
     </DailyProvider>
   </StrictMode>
