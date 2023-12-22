51ea970b800ef8664e18a0d441b098c346c14d83
James Hush <james@daily.co>
2023-12-22 16:25:48 +0800

    Remove mkcert

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import mkcert from "vite-plugin-mkcert";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   server: { https: true }, // Not needed for Vite 5+
-  plugins: [mkcert(), react(), eslint()],
+  plugins: [react(), eslint()],
 });
