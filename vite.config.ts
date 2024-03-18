092a2c2dd9e58c636be44227ee0646d1dcd9ca9f
James Hush <james@daily.co>
2024-03-18 18:01:09 +0800

    chore: fix codesandbox integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import eslint from "vite-plugin-eslint";
-import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  server: { https: true }, // Not needed for Vite 5+
-  plugins: [mkcert(), react(), eslint()],
+  server: {
+    port: 3000,
+  },
+  plugins: [react(), eslint()],
 });
