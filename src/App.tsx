9b3fa7c452201ae94a2695c815f368c3ae7c2c0d
James Hush <james@daily.co>
2024-04-12 13:07:25 +0800

    Fix typos

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ export default function App() {
   const [inputSettingsUpdated, setInputSettingsUpdated] = useState(false);
   const [enableBlurClicked, setEnableBlurClicked] = useState(false);
   const [enableBackgroundClicked, setEnableBackgroundClicked] = useState(false);
-  const [dailyRoomUrl, setDailyRoomUrl] = useState("https://hush.daily.co/sfu");
+  const [dailyRoomUrl, setDailyRoomUrl] = useState("");
   const [dailyMeetingToken, setDailyMeetingToken] = useState("");
 
   const {
@@ -386,7 +386,7 @@ export default function App() {
         </button>{" "}
         <button
           onClick={() => {
-            // App messages aren't sen't to the local user, so we'll play the sound here
+            // App messages aren't sent to the local user, so we'll play the sound here
             new Audio("https://jameshush.com/arrow.mp3").play().catch((err) => {
               console.error("Error playing sound", err);
             });
