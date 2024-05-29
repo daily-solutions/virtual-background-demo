3f8f21a26cc3696d5a10258b828a8205498e42a7
James Hush <james@daily.co>
2024-05-29 14:45:18 +0200

    Fix lint and types

--- src/Prebuilt.tsx
+++ src/Prebuilt.tsx
@@ -2,10 +2,84 @@ import "./styles.css";
 import { useCallback, useEffect, useRef } from "react";
 import {
   DailyProvider,
+  ExtendedDailyParticipant,
   useCallFrame,
   useDaily,
   useParticipantCounts,
+  useParticipantIds,
+  useParticipantProperty,
+  usePermissions,
 } from "@daily-co/daily-react";
+import { DailyEventObjectParticipant } from "@daily-co/daily-js";
+
+const ParticipantRow = ({ sessionId }: { sessionId: string }) => {
+  const participantName = useParticipantProperty(sessionId, "user_name");
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const canSendAudio = usePermissions(sessionId).canSendAudio;
+  const canSend = typeof canSendAudio === "boolean" ? canSendAudio : false;
+
+  const canSendAudioParticipantIds = useParticipantIds({
+    sort: useCallback(
+      (a: ExtendedDailyParticipant, b: ExtendedDailyParticipant) => {
+        const aLastActive = a.last_active ?? 0;
+        const bLastActive = b.last_active ?? 0;
+
+        return aLastActive < bLastActive ? -1 : 1;
+      },
+      []
+    ),
+    filter: useCallback((p: ExtendedDailyParticipant) => {
+      const canSend = p.permissions.canSend;
+      if (typeof canSend === "boolean") return canSend;
+      return canSend.has("audio");
+    }, []),
+    onParticipantUpdated: useCallback((p: DailyEventObjectParticipant) => {
+      console.log("Participant updated", p);
+    }, []),
+  });
+
+  // Change to 15 later
+  const isStageFull = canSendAudioParticipantIds.length > 2;
+
+  const callObject = useDaily();
+  return (
+    <ul>
+      <li>{participantName}</li>
+      <li>
+        {isStageFull ? (
+          <p>Stage is full</p>
+        ) : (
+          <button
+            disabled={canSend}
+            onClick={() => {
+              callObject?.updateParticipant(sessionId, {
+                updatePermissions: {
+                  canSend: new Set(["audio", "video"]),
+                },
+              });
+            }}
+          >
+            Allow Audio
+          </button>
+        )}
+      </li>
+      <li>
+        <button
+          disabled={!canSend}
+          onClick={() => {
+            callObject?.updateParticipant(sessionId, {
+              updatePermissions: {
+                canSend: new Set(["video"]),
+              },
+            });
+          }}
+        >
+          Deny Audio
+        </button>
+      </li>
+    </ul>
+  );
+};
 
 const App = () => {
   const callObject = useDaily();
@@ -13,12 +87,29 @@ const App = () => {
   // @ts-expect-error debugging
   window.callObject = callObject;
 
+  const allParticipants = useParticipantIds({
+    sort: useCallback(
+      (a: ExtendedDailyParticipant, b: ExtendedDailyParticipant) => {
+        const aLastActive = a.last_active ?? 0;
+        const bLastActive = b.last_active ?? 0;
+
+        return aLastActive < bLastActive ? -1 : 1;
+      },
+      []
+    ),
+  });
+
   const participantCount = useParticipantCounts();
 
   return (
-    <>
-      <span>{participantCount.present} participants</span>
-    </>
+    <ul>
+      <li>{participantCount.present} participants</li>
+      <button>Raise Hand</button>
+      <button>Lower Hand</button>
+      {allParticipants.map((participantId) => (
+        <ParticipantRow key={participantId} sessionId={participantId} />
+      ))}
+    </ul>
   );
 };
 
@@ -42,9 +133,14 @@ export const Prebuilt = () => {
 
   useEffect(() => {
     if (!callFrame) return;
-    callFrame?.join().catch((err) => {
-      console.error("Error joining call", err);
-    });
+    callFrame
+      ?.join({
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyIjoiZGVtbyIsInAiOnsiY2EiOnRydWV9LCJkIjoiNjgzYjg5ZjQtYjFmZC00Zjk0LWI4NDUtZWYyNjY2ZTNlMjEzIiwiaWF0IjoxNzE2OTgzNzc3fQ.s4_L9chKyALjGk3BmgH76Q8Dy1n5zZJ6NI-VF0dURL8",
+      })
+      .catch((err) => {
+        console.error("Error joining call", err);
+      });
   }, [callFrame]);
   return (
     <DailyProvider callObject={callFrame}>
