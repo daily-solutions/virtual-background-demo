1e3e961bad5573f89de7bafbe5e994902a8ba911
James Hush <james@daily.co>
2024-06-12 13:35:21 +0200

    Better

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,40 @@ import { Prebuilt } from "./Prebuilt";
 import { DailyProvider } from "@daily-co/daily-react";
 import App from "./App";
 
-import * as f from "pixi-live2d-display";
+import * as PIXI from "pixi.js";
+import { Live2DModel } from "pixi-live2d-display";
 
-console.log(f);
+// expose PIXI to window so that this plugin is able to
+// reference window.PIXI.Ticker to automatically update Live2D models
+// @ts-expect-error PIXI is not normally on the window object
+window.PIXI = PIXI;
+
+(async function () {
+  console.assert();
+  const view = document.getElementById("canvas") as HTMLCanvasElement;
+  const app = new PIXI.Application({
+    view,
+  });
+
+  const model = await Live2DModel.from("shizuku.model.json");
+
+  app.stage.addChild(model);
+
+  // transforms
+  model.x = 100;
+  model.y = 100;
+  model.rotation = Math.PI;
+  model.skew.x = Math.PI;
+  model.scale.set(2, 2);
+  model.anchor.set(0.5, 0.5);
+
+  // interaction
+  model.on("hit", (hitAreas: any) => {
+    if (hitAreas.includes("body")) {
+      model.motion("tap_body");
+    }
+  });
+})();
 
 const container = document.getElementById("root");
 
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,4 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [react()],
-  optimizeDeps: {
-    include: ["pixi-live2d-display"],
-  },
 });
